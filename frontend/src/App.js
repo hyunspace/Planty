2af3c3f4bdfe0f4addcc61ec3f60b4160889f705
조현빈 <jo7hb@naver.com>
2022-09-05 17:06:24 +0900

    ✨ Create NavBar
    - create navbar with routing
    
    
    Former-commit-id: 64e1b2d643d2ff06b1c7fd8637857d389b89a453

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="" element={<IndexPage />} />
-    </Routes>
-  );
-};
-
-export default App;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6bc0116c7ed6454ecd4fa62e7cf5d9661cbfc83d2e373b132432c6fb41c7880b
+size 620
