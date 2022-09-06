90ee156b1c69db668acb924a36570bb2fd519145
Hong Seong Mok <swff07183@gmail.com>
2022-09-06 16:24:24 +0900

    ✨ Dictionary 페이지
    - Dictionary 페이지 데스크탑 디자인
    
    
    Former-commit-id: 82527eec049fed9e037134c25dafa18204894882

--- frontend/src/pages/DictionaryPage.js
+++ frontend/src/pages/DictionaryPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:782a2df34c70ac846fbb3dfe0fd8fc0a57186fdc4c4f5bd22aefca8ddb9766d4
-size 787
+oid sha256:ee016396ff1b2951f144f1fdcc2fea89086293e5514da048d61a8b12acaa15f5
+size 2837
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92feba7bf3af502a4807b995fd422e846bb284fe1791dbd1d9adab9fbd9e2a01
-size 113
+oid sha256:9657d3ace8ff7731314ff3fbd65f005a4900e24bfeffb9a4e96ec0e31a7ea777
+size 232
