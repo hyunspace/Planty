be5001d1f8e64fd796af6c522387113d0a344014
JangJiseon <jsmeaningood@gmail.com>
2022-09-07 17:26:35 +0900

    ✨ User model custom
    - ERD 수정에 따른 유저 모델 커스텀
    
    
    Former-commit-id: 43a65518be8cbe6a45ad699e2f4768a14d8e8707

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    pass
+version https://git-lfs.github.com/spec/v1
+oid sha256:971ee920a1b99c06872e66396832754f02dc402d5e8736e9c6eb9a09b63a002f
+size 519
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1f0053f96fd94093d34fe16bfd7286d133825fa38ffaff7ea8c53964bde3f49
-size 3923
+oid sha256:2b70a1082325e611afc6ac05b1d69c5c72c9560ac342153a67e17256d0fbeedf
+size 4098
