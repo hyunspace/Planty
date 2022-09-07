c18bd504b3782923cdc0e35c209a3e4b5aea3883
조현빈 <jo7hb@naver.com>
2022-09-07 17:05:38 +0900

    ✨ Card Component, Horizontal Scroll 구현
    - Card Component, Horizontal Scroll 구현
    - React Horizontal Scroll Menu 사용
    
    
    Former-commit-id: 7e485fdad2e0c4ad7937e07b13d4aa4e5a48fd64

---
+++ frontend/src/components/Arrow.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d52b60fc1c7dddc01fab451cd84fe9c28f943b76ad9922a05a9e66e87592855b
+size 1910
---
+++ frontend/src/components/index/Card.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c937065bc516fbd3e720cc19ce5606c3906bebf3294069a5cb84c556011101c
+size 1209
