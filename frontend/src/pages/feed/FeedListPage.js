514106ab18d2cb669571e481182978ab8bb05b80
Hong Seong Mok <swff07183@gmail.com>
2022-09-08 17:25:11 +0900

    ✨ Feed Modal 추가
    - Feed Modal 추가
    
    
    Former-commit-id: a749eff208e870ed65148e3c44035b04889d4abb

--- frontend/src/pages/feed/FeedListPage.js
+++ frontend/src/pages/feed/FeedListPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:275c4a26489e610cdb0c313ba0642253de37ce8041ee673982a3fa2614466561
-size 986
+oid sha256:c8c9971fe0491182fabae26715284305d0bf8d1bb81e4794e32ac191f2ecf383
+size 1039
