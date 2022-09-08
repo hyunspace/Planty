060ace484ab7f0b3ef28eb80d37b624bc42afdcf
조현빈 <jo7hb@naver.com>
2022-09-08 10:26:13 +0900

    ✨ Finished indexpage layout
    - 메인페이지 레이아웃 작성
    
    
    Former-commit-id: 430e77b920462dc0de752f26f63306a91e822cd6

--- frontend/src/components/TagButton.js
+++ frontend/src/components/TagButton.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a64821c258e00f6f65d5ecc65ecbb929b2258faa73fc7859e10a48b8ab2a3f2d
-size 434
+oid sha256:178ddd463736140d3e07c4ffc49e118273f2c81e383aa71b477dfecacf05aaa6
+size 419
--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4e114bc28534f0783678038a6151329c1c3e57980ebd02e8f9a26becf80688c
-size 2130
+oid sha256:9fd414a2b49af6647543d3150b0d9a4e6f5e465e261475f356f60a47b334eb0e
+size 3432
