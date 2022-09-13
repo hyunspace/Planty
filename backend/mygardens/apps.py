78fe8b67b6dfb7eb8726052cb8eceb35e86e346c
Hong Seong Mok <swff07183@gmail.com>
2022-09-13 21:08:37 +0900

    ✨ 남의 정원 모바일 레이아웃
    - 남의 정원 모바일 레이아웃
    
    
    Former-commit-id: 2203f9d0fad664a33e8205e4e8ea71847d2ad0a7

--- backend/mygardens/apps.py
+++ backend/mygardens/apps.py
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b945bdf5a33a339aba1d9b6f240ecab69cc955be77b10a7853023d96eb6f7888
-size 150
+from django.apps import AppConfig
+
+
+class MygardensConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mygardens'
