eb786f6e4dd0bb5a30126abb6bcfa26086c4edf5
hyunspace <33j073@gmail.com>
2022-09-14 17:54:11 +0900

    ✨ BE Plant Modeling & Viewset
    - MySql 연결 후 식물 정보 테이블 생성
    - 식물 사전 ReadOnlyViewset 생성
    
    
    Former-commit-id: 797d82561da38e7dda7eb9b3997b4c188c3bc35f

--- backend/plants/views.py
+++ backend/plants/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7523ccf8e77d1aec8801d1c2f98142a2cf8cf8eca3138d4e75d6f400affbe24
+size 355
