4c111fbc96f98a509bed409236c6ef474c68dee2
조현빈 <jo7hb@naver.com>
2022-09-16 16:20:33 +0900

    ✨ 페이지네이션 완성
    - 페이지네이션 기능 완성
    - 서버와 테스트 완료
    
    
    Former-commit-id: 58fa7712f0566353e04e00050478411e9bfc5ca4

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24a86959062a27ae334e7fae00e6ffd6e057583a576b6073b44a02b6d9379183
-size 3432
+oid sha256:8b9a2beed94150ef5db84ab7e88bf37688defb265a76a0e83bffdb83abaeccc0
+size 3459
