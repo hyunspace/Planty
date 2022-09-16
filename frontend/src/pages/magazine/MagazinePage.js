237c303150c62f4a2c7133fbd034911a173a9409
조현빈 <jo7hb@naver.com>
2022-09-16 16:20:33 +0900

    ✨ 페이지네이션 완성
    - 페이지네이션 기능 완성
    - 서버와 테스트 완료
    
    
    Former-commit-id: 4f56c20a00a05460ac8a7fb66125c97206b60c21

--- frontend/src/pages/magazine/MagazinePage.js
+++ frontend/src/pages/magazine/MagazinePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ff65f06342b22b786445eb0a6b5fce91b54f280d600f74b8d9111ca4c7fcad3
-size 8622
+oid sha256:aa048511a9ed992fb25831a149449b7044bb8853a107aa96a710763b17b7cd56
+size 12878
