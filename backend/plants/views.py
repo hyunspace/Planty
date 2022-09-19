73c940c39ee140557de447b4bb38c134e07b617a
hyunspace <at.hyunspace@gmail.com>
2022-09-20 01:20:13 +0900

    ✨ 식물 사전 초기 목록 및 상세 페이지
    - 식물 사전 초기 목록
    - 식물 사전 상세 페이지
    - 식물 사전 검색 기능(-ing)
    
    
    Former-commit-id: 5f87e07396af3d93ddff7330756082596cdec13c

--- backend/plants/views.py
+++ backend/plants/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from .models import Plant
-from rest_framework import viewsets, status
-from .serializers import PlantDictSerializer
-
-# Create your views here.
-class PlantDictViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Plant.objects.all()
-    serializer_class = PlantDictSerializer
+version https://git-lfs.github.com/spec/v1
+oid sha256:40cfe9eaaa91b4905e6809236f8192907f1e61ceedeceae0178599d4deed712b
+size 1650
