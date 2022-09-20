dc410a117f52fd42c710980b48e551cd48e01284
JangJiseon <jsmeaningood@gmail.com>
2022-09-20 23:40:15 +0900

    ✨ Magazines bugfix
    - Magazines bugfix
    
    
    Former-commit-id: 9f5ad55a8d967a66bcb8e8c2d0993bfb789bf154

--- backend/mygardens/models.py
+++ backend/mygardens/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a655d9f0bffc68badd9fcc3ac3d0a14fdf40a2732efed75e51db219e82043455
-size 1010
+oid sha256:26637986a90e0ac119e8ea3397d33f1c75520bfabbe2df5de264c448e3d38892
+size 1572
