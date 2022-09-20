9810647dff2368e738bc183c578480f758a887ae
JangJiseon <jsmeaningood@gmail.com>
2022-09-21 00:00:57 +0900

    ✨ magazines bugfix2
    - magazines bugfix2
    
    
    Former-commit-id: 99c4068e0807fe54b4cd8f86632470b07338e4ea

--- backend/magazines/serializers.py
+++ backend/magazines/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d7887153e3fb985d076168c6d70eaad656c2e1e61bf36e4545036f9d552a2bb
-size 1152
+oid sha256:412ccace582938765186076eb8cb7253d60e34a08db16cc74985008d40f6cde5
+size 918
