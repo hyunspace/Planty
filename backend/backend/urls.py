093abbe42ef7e0b3d091e0a577ba9493a946d056
Hong Seong Mok <swff07183@gmail.com>
2022-09-20 09:08:06 +0900

    ✨ aws s3 파일 업로드 함수 추가
    - aws s3 파일 업로드 함수 추가
    - secrets.json 분리
    
    
    Former-commit-id: cf81e7ea2c99286e7965fa9055d5b9c1eb5e44b5

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:495208aba249ee58e1869cb46a7558cc11caf6a845f94588f1d8db5089655c53
-size 1723
+oid sha256:6a64be81045b2e3ef554852ca7060514866bd79b6712749da07fa7febfc0dcef
+size 1778
--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:7012d3373ca7af2313ae4e5323f5d9aef4a8c3663c38fc865ed9d55496c3bc52
+size 725
