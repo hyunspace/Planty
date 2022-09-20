dfd5d01d35d174ea0d14ec26a8afea90f0162571
JangJiseon <jsmeaningood@gmail.com>
2022-09-20 18:13:00 +0900

    ✨ 읽을거리 댓글 CRUD, Like 완료
    - 읽을거리 댓글 CRUD, 좋아요 완료
    - User 테이블에 개수 카운트 완료
    
    
    Former-commit-id: d77f41924a86e47cef2539506f59496f4d3c6570

--- backend/magazines/migrations/0001_initial.py
+++ backend/magazines/migrations/0001_initial.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11081b58c5b24d81bd1116e1be2e695764f5419352ee69c3afe8dd8922e481bc
-size 3238
+oid sha256:825e90e3f9308aafad2c33ad38f86387bf949837b7e8abc0b6c640697ff7b955
+size 3288
