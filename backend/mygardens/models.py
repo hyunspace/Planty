babcee84febf4068b695d814c620b3e7dab87dcb
Hong Seong Mok <swff07183@gmail.com>
2022-09-20 09:08:06 +0900

    ✨ aws s3 파일 업로드 함수 추가
    - aws s3 파일 업로드 함수 추가
    - secrets.json 분리
    
    
    Former-commit-id: 25a65462c002c9857a5a6442dc22cdc0e2ea6026

--- backend/mygardens/models.py
+++ backend/mygardens/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e3dc924a8588fcc0c3548dc5517846e215708fc61529410af5ea17c8eba69ce
+size 1025
