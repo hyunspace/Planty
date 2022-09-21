71b5362efc61ddb5f2b5c6a8c99b7add887bfd21
JangJiseon <jsmeaningood@gmail.com>
2022-09-21 11:33:55 +0900

    🔨 accounts date_of_birth
    - null=True
    
    
    Former-commit-id: ba9e2f2e8d5b0e79b6d7b8d81c5c2d69dec7988b

--- backend/mygardens/urls.py
+++ backend/mygardens/urls.py
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a2fbc12fdc92368f7381b1594695fea7a7c54d04749dab28f013165c4d639e76
-size 242
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r"", views.GardenViewSet, basename="")
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
