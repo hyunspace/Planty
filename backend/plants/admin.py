321af51a96082c6c6dfaec588f5b9cdcfc9b3d8e
hyunspace <33j073@gmail.com>
2022-09-21 11:36:45 +0900

    ✨ Admin 페이지 Plant 추가
    - 관리자 페이지에서 Plant 관리 기능 추가
    
    
    Former-commit-id: 0f81dfcf0e8136920275dcccf777237b792d3aff

--- backend/plants/admin.py
+++ backend/plants/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:9575b698fabbde8f6d98469fbfda751e30138b71bc97e6957a070b6ddaa67178
+size 170
