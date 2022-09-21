e62442b61f5e97dc77f1705be9703b834a376bde
조현빈 <jo7hb@naver.com>
2022-09-21 09:40:48 +0900

    🔨 메인페이지, 읽을거리, 식이월 반응형
    - 메인페이지, 읽을거리, 식이월 반응형 수정
    
    
    Former-commit-id: 5f9ba9569c4b1ea22cf0ed6e3e27f8ff6456ff49

--- frontend/src/pages/magazine/MagazineDetailPage.js
+++ frontend/src/pages/magazine/MagazineDetailPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b1d10ba9e8f1d271ecd83d37369ca09447e75a464eb482d28488d938b34629
-size 4666
+oid sha256:8d4b749411424aeecbc08cda997339f84a168a0c77a03155cb215d00e82685d8
+size 4973
