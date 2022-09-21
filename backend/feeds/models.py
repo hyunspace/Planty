8c43a8d012fe83f670d3fd71e4abbd7b17a5e868
JangJiseon <jsmeaningood@gmail.com>
2022-09-21 11:00:16 +0900

    ✨ 남의 정원 모델링, CRUD, 댓글, 좋아요
    - 남의 정원 모델링, CRUD
    - 댓글, 좋아요
    
    
    Former-commit-id: 0f91466832bc68bff2c38ba81c1839f4928acb38

--- backend/feeds/models.py
+++ backend/feeds/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c21b4f5f1098d95d312887d6ff47ad4b8acbf48c0d01f6fdc4485b31d1e4060
+size 2017
---
+++ backend/feeds/serializers.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:951e35a21dc05b5097c3b8e10e23416a09130bfa2706ee78f9459927beb88b05
+size 975
