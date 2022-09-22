1f19aa1159e72a58fc0041690e7f0cf2c0e06059
조현빈 <jo7hb@naver.com>
2022-09-22 17:02:48 +0900

    ✨ 읽을거리 검색, 글쓰기, 필터링 기준
    - 읽을거리 검색, 글쓰기, 필터링 기준 등록
    - 반응형 개선
    
    
    Former-commit-id: 3e65b1d1b9c2d702a4f7c869535fec195650d50f

--- backend/magazines/serializers.py
+++ backend/magazines/serializers.py
@@ -1,8 +1,3 @@
-from rest_framework import serializers
-from .models import Magazine
-
-
-class MagazineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Magazine
-        fields = "__all__"
+version https://git-lfs.github.com/spec/v1
+oid sha256:4948aaa06107fdf104cdb5cf033d12daf0074537f2f7f04948ea3005c6deec17
+size 193
--- frontend/src/pages/magazine/MagazinePage.js
+++ frontend/src/pages/magazine/MagazinePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a03c5e739e53b325e4b70ff507ee65113976b6c96e49534a27331c6c5754fed3
-size 7815
+oid sha256:a33e1ebd3b07cf5a41e1c33653c876a71be5df2ba7e5e0f72c5a4c2d8c935f6b
+size 10464
