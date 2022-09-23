1a0c91ae46e8e8e2c1fb10c308e861b717af6048
Hong Seong Mok <swff07183@gmail.com>
2022-09-23 13:37:46 +0900

    prod setting test
    Former-commit-id: 09d417ccc2b7324b63dd0b362a0a3c96188d7129

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a509c3ac8e18477b2b6ee47257322f9cb73095aa0e3898beee58444ea874754
-size 5335
+oid sha256:ac6a6f19e4c15554650bda557afd15f4fa3be789b2823c89b4365c87177577ac
+size 5338
