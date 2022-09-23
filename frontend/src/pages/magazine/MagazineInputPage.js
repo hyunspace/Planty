32f8a3f67a00bace4fbd7f11146cb2c188fc5956
조현빈 <jo7hb@naver.com>
2022-09-23 22:08:32 +0900

    ✨ 읽을거리 글쓰기
    - 읽을거리 글쓰기 페이지 추가 및 반응형 레이아웃 작성
    
    
    Former-commit-id: a18883d5e9bacd8ff046d51535d68a51ddd0ada0

--- frontend/src/pages/magazine/MagazineInputPage.js
+++ frontend/src/pages/magazine/MagazineInputPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68d27da23173c881fef16da5c9ec9961ba89f52d8b6391623481669632c3c1f6
-size 1059
+oid sha256:61fa489facf67954073ad3cba5cea0d6146842ecd19ec683d133280fd2fc086d
+size 4520
