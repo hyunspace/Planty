5b9fdd649d6ddd8a4b09a2c43a32e362c815ad5a
Hong Seong Mok <swff07183@gmail.com>
2022-09-26 00:58:26 +0900

    ✨ 회원가입, 로그인 반응형
    - 회원가입, 로그인 반응형
    - feed 반응형 수정
    
    
    Former-commit-id: 8fa33057355a0718d2b7b802460614efcea328f9

--- frontend/src/components/feed/FeedModal.js
+++ frontend/src/components/feed/FeedModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1f0e02585976e6c92e3b7c9f60ce1d49172933928c98c5d0160c57b4f897b21
-size 11919
+oid sha256:e10a1c444baa64ce1725895ed9f19ca0a5e7c11baa014c197d5e8d69655665a3
+size 11990
--- frontend/src/pages/user/RegisterPage.js
+++ frontend/src/pages/user/RegisterPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:355a53376540c20f81d601e44c3c94fe0cf2e0b9888158bbefd1069449f25094
-size 5883
+oid sha256:4563837becd49fabf8436aac385ec133376650f9a73eb9096e2eeb9715ca29e8
+size 7592
