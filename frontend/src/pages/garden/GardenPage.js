ba7a869831547e2372677f241cc3ae74c5424202
조현빈 <jo7hb@naver.com>
2022-09-26 15:42:29 +0900

    ✨ 나의 정원 반응형 수정 및 API 요청
    - 게시물 크기 반응형 수정
    - API 데이터 요청 확인
    
    
    Former-commit-id: 9559711d64a7ee6c08d3b0d1ac2379fe70cec534

--- frontend/src/pages/garden/GardenPage.js
+++ frontend/src/pages/garden/GardenPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d97a118030dd1567d17ddb080ca174862975ef8d05b08886a8c11b273c0d4092
-size 2898
+oid sha256:5e1adb7501954d2856d7c7dbdba715c11b472c0422a13f3757cb474281e0fb61
+size 3703
