a45169615f0d5cc528eaf1661d29d85e5c26fccc
홍성목 <swff07183@gmail.com>
2022-09-26 16:14:35 +0900

    ✨ 식물
    - 반응형 수정
    
    
    Former-commit-id: e15076edec15215c2510989fb8f907569625f81c

--- frontend/src/components/feed/FeedItem.js
+++ frontend/src/components/feed/FeedItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d21cf7e8acb2caaf7270877fa8606a91b214b8bed0411e21c2b864fb7b33cd9
-size 2302
+oid sha256:9bc882da785704ff9b66cd9071493d294bb3fd783b013cd37df15d74a3c0ac1f
+size 2322
