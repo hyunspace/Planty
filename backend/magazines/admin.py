00eff70505356a1a0ea23ad36bb0a7fe69c60b14
Hong Seong Mok <swff07183@gmail.com>
2022-09-27 22:25:32 +0900

    ✨ magazine 페이지네이션 추가
    - magazine 페이지네이션 추가
    
    
    Former-commit-id: dfc6e61b3b8b8663e992aa846a4fb3f6dba11031

--- backend/magazines/admin.py
+++ backend/magazines/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8f20e421de579bcfb2dcca4218c8ad810bc5c7496bba51f9e6e2d1ee35bece3
+size 121
