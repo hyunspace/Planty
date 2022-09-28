8e1fbffb39544c3308a56816e75893aaf1e69788
JangJiseon <jsmeaningood@gmail.com>
2022-09-28 15:59:03 +0900

    package-lock
    Former-commit-id: a83709038244a7f5c748888fdc2a47771d2e563a

--- frontend/src/api/garden.js
+++ frontend/src/api/garden.js
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9c0a2d822913214ed9de9a63413b86007f65d19cbf5f1e6a400b2e2ce8d37f96
-size 165
+import client from './client';
+
+export const gardenUser = async (userName) => {
+  const response = client.get(`accounts/profile/${userName}`);
+  return response;
+};
--- frontend/src/features/feed/feedSlice.js
+++ frontend/src/features/feed/feedSlice.js
@@ -1,23 +1,3 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchFeedList, fetchFeed, createFeed } from './feedAction';
-
-const initialState = {
-  loading: false,
-  feed: null,
-  feedList: null,
-  error: null,
-  success: false,
-};
-
-const feedSlice = createSlice({
-  name: 'feed',
-  initialState,
-  reducers: {},
-  extraReducers: {
-    [createFeed.pending]: (state) => {},
-    [createFeed.fulfilled]: (state) => {},
-    [createFeed.rejected]: (state) => {},
-  },
-});
-
-export default feedSlice.reducer;
+version https://git-lfs.github.com/spec/v1
+oid sha256:eee931a51f6af6df521d17c74bfb63ab1ef03008573c05b3ed8f8184349677f0
+size 497
