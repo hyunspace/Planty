74bfdc9bef6e31f972254561e56ae3108bbf16cb
JangJiseon <jsmeaningood@gmail.com>
2022-09-28 13:48:33 +0900

    ✨ 나의 정원 선물용 컬럼 추가
    - 나의 정원 선물용 컬럼 추가
    
    
    Former-commit-id: 6f4f7791cffb7e4528b577349c8da0a4fc40b8ce

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe347eda5eccdcac26ee0a803b664e84401be5e69ece90d935c98fdf4e9eafa
-size 4888
+oid sha256:1bc23849c6a42cf55bcf88712571812b0426c97463d549557f6b5c1e0bc45497
+size 5409
