56af9674056ef3aaf2b2765b726d039b5c4496c3
홍성목 <swff07183@gmail.com>
2022-09-28 15:07:22 +0900

    ✨ 피드 수정
    - 피드 수정
    
    
    Former-commit-id: 9b676165498a9fcb965caa37f8e4dec1e80e1d9a

--- frontend/src/components/feed/FeedModal.js
+++ frontend/src/components/feed/FeedModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a904408b344860e37fc5be16ee34cbd764e35e7240091b982c2e8d1bce0e05fd
-size 12336
+oid sha256:4f0e9c7e10b98ef087bed4b1ed94f9168488c1633f233d11b14cc4060884ba4f
+size 12540
