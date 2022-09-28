9f80f2b641f95dc8ae3e606ebd132450632ceefa
hyunspace <33j073@gmail.com>
2022-09-28 13:51:58 +0900

    ✨ Recommendations 앱 생성
    - Recommendations 앱 생성
    - UserKeywordCount 모델 생성
    - UserKeywordCount 테이블 테스트 데이터 추가
    
    
    Former-commit-id: d90fe1911097d93e7bf63dbeb05080e659ea6cd7

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94818871119db49ac5f5c8b527018d2d4e2b5d7b0567d0e16f15b5d7dc5fd3a1
-size 5266
+oid sha256:95a4483036f32c426d72490dbf129b944ab416c87d57f1a0e2ee59c834035006
+size 5289
