8f5bc85347ae1eeb7898f9f0f6ed6d27cd8254ac
Hong Seong Mok <swff07183@gmail.com>
2022-09-29 12:27:54 +0900

    ✨ 읽을거리, 피드 좋아요 여부, 댓글목록
    - 읽을거리 댓글목록, 피드 좋아요 여부
    - 읽을거리 좋아요 여부
    
    
    Former-commit-id: 76daa11019d1b8e188c384efc5f1679a039e59b3

--- backend/magazines/views.py
+++ backend/magazines/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8050566ae29a14b8065b958e951d4068b7b1cc5ef4c6be8f0ca9179069a0a30
-size 6482
+oid sha256:207c996fd3479798c9da697d0c2f34048df107757812562a8d1e57fed9a61e8a
+size 6664
