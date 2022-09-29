eb8de6c94bbf83684e59b66f5bd6c78f04949251
조현빈 <jo7hb@naver.com>
2022-09-29 11:53:04 +0900

    ✨ 읽을거리 API 통신(1)
    - 읽을거리 리스트 받아오기
    - 읽을거리 글쓰기
    - 읽을거리 좋아요
    
    
    Former-commit-id: 58bedd362cb31a86259386c0c93ba9d1ecf06af9

--- frontend/src/api/garden.js
+++ frontend/src/api/garden.js
@@ -1,6 +1,3 @@
-import client from './client';
-
-export const gardenList = async () => {
-  const response = client.get('mygardens');
-  return response;
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:c50df1ac1a1d25fed5ec0bbd146f2bc85c74346c06fa18f4fdb2e8faa57e8457
+size 138
---
+++ frontend/src/features/magazine/magazineSlice.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84552c9c322132a07d8963410499fd59951cf153da91b6b57cbe0cd04965b9ea
+size 1296
