97256ebeb7d57e719b5374e6a1f5086a81dc426d
Hong Seong Mok <swff07183@gmail.com>
2022-09-29 11:56:51 +0900

    ✨ 식물 페이지네이션 추가
    - 식물 페이지네이션 추가
    
    
    Former-commit-id: 75292debf32250f1c79f7d45df1ff38a8dbe7527

--- frontend/src/pages/dictionary/DictionaryPage.js
+++ frontend/src/pages/dictionary/DictionaryPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f429357a854ce41e592911cb641f3e9c98223f3104c298532fec1c1a64b70a55
-size 6764
+oid sha256:dca0afd197578d6cce6c608acda9350742ad241985cd1e60c3ac863cf06b8174
+size 6822
