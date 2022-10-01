3541a58537e4df996a1585b98e16139206a9e4b3
hyunspace <at.hyunspace@gmail.com>
2022-10-02 00:00:16 +0900

    ✨ Recommendations 모델 수정
    - Recommendations models.py 수정(NAS DB 기준)
    
    
    Former-commit-id: d226859739489e9bbfb0259c708b5527ab1bff6e

--- backend/recommendations/serializers.py
+++ backend/recommendations/serializers.py
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d6b37731ed7c1bc2b28c41dd76d770e605b50e7a0e21882ff6c6c5ba313a742b
-size 253
+from rest_framework import serializers
+
+from plants.models import Plant
+from .models import UserKeywordCount
+
+class UserKeywordCountSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = UserKeywordCount
+        fields = ('__all__')
