884b8be315862083729d6d58c48204928a99de0f
JangJiseon <jsmeaningood@gmail.com>
2022-10-01 17:13:52 +0900

    ✨ 나의 정원 식물 수정(patch)
    - 메모, 물주기, 선호도 수정
    
    
    Former-commit-id: a50dd0b5bcb183dc5c2eec220b5496feb71403c6

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94c4d89855274905ffa4b09a705163c47a762f367b18e9a895e95b3bf6a0e48c
-size 5897
+oid sha256:817ec0ea8c466049e4139da09e0b2cad3589517fc00aaed895334c07d01d0867
+size 7525
