6ceb398f08a7edfc9e0b102bf9043cc545c990e3
hyunspace <at.hyunspace@gmail.com>
2022-10-02 02:07:17 +0900

    📝 Plants, Recommendations 모델 업데이트
    - verbose_name 추가
    - 각 앱의 불필요한 inspectdb 파일 삭제
    
    
    Former-commit-id: 322f52292c537f5e51a6e0eb684c75845574e206

--- backend/recommendations/models.py
+++ backend/recommendations/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3df9920d4adbbaf57bd68f3d3df8e9ffd643338f6ca19550b03ab4fba8f09a
-size 2650
+oid sha256:d49c682127f4cef9d953dc4e7c8b0c4e2edb7a2f3baa45b5a5b3bb4145a71ad8
+size 3257
