d06f05df037300749c20d268364605050cf6e32a
JangJiseon <jsmeaningood@gmail.com>
2022-10-02 00:26:31 +0900

    ✨ plant popular 추가
    - plant popular 추가
    
    
    Former-commit-id: 19323f972540f1db2ee4fc01b3efaaaa75954fc6

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a989556192483f5b6f2878a91fbd96961101dadeeaa978db4f1bbaab2ee1ce1d
-size 14213
+oid sha256:2276605d32e93fa0134be839f50153365a9401c91f9aaf54292f0cd6e2ca8893
+size 14271
---
+++ backend/plants/model2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ec0aa5e3681f03ed3e8381ac966543b60d6d726a0a64b33fc298bc6706b97a8
+size 17837
