153f206eb9b0870ef2b864b56c8244cc5a69a969
hyunspace <at.hyunspace@gmail.com>
2022-10-02 01:58:27 +0900

    🚨 UserKeywordCount 추가용 테스트 코드
    - MyGarden/views.py에 추가할 UserKeywordCount 테스트 코드 완성
    
    
    Former-commit-id: 702233c875b284d2a9f62aee26c63c338182b1c7

--- backend/recommendations/views.py
+++ backend/recommendations/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e59b855ad4716a0fbc93c6360bae2ff91e5eb6b6ab408b7f39a4e909c719d7ed
-size 7666
+oid sha256:3bff9a6a9d1f46de4975795bb35ec794a2e77fe47b2c6f455a28f89bab911145
+size 5780
