dfc1e8f47efe6e139b5b9309036b2a3696cc9928
JangJiseon <jsmeaningood@gmail.com>
2022-10-02 02:00:45 +0900

    ✨ 반려동물에게 안전한 식물
    - 반려동물에게 안전한 식물 랜덤 8개
    
    
    Former-commit-id: db24fb884e151d96bf6e4a5b13d4be6744fc1e75

--- backend/plants/urls.py
+++ backend/plants/urls.py
@@ -1,11 +1,3 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-
-app_name = 'plants'
-
-router = DefaultRouter()
-router.register(r'', views.PlantViewSet, basename='')
-urlpatterns = [
-    path('', include(router.urls))
-]
+version https://git-lfs.github.com/spec/v1
+oid sha256:19acd62f9e33867a655ede9e1e9b0c309d5230cd2658c62290d87b04cd432f5f
+size 333
