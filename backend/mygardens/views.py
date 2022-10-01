cd0c7ddcc79922b3a0b9160a2be1e5c474cf1dcd
JangJiseon <jsmeaningood@gmail.com>
2022-10-01 20:31:43 +0900

    🔨 주석 수정
    Former-commit-id: ba10efb63153948dcdde6a78761d151c2224a655

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb91b4bf64643170592c63ce30272e5f51f32b8aeef6dcac47c65e657d8489a3
-size 10091
+oid sha256:809c66f3ef1efb49370af0b3440177151014c013676105bada80bcc8f2ed60f9
+size 10089
--- backend/plants/model2.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:097cb4be02d1511213fc0e1b011da55d39223c6d17a30c2344572599adec203e
-size 17801
