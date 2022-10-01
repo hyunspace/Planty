d869cb1d1c6101500e9a050c1ab9d7832d6c1a80
JangJiseon <jsmeaningood@gmail.com>
2022-10-01 16:26:09 +0900

    ✨ 나의 정원 키워드 카운트
    - 나의 정원 키워드 카운트
    
    
    Former-commit-id: f482a2ebd51a9240c7092e0ca813f7cd4c3a1db5

--- frontend/src/api/garden.js
+++ frontend/src/api/garden.js
@@ -1,6 +1,3 @@
-import client from './client';
-
-export const gardenUser = async (userName) => {
-  const response = client.get(`accounts/profile/${userName}`);
-  return response;
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c0a2d822913214ed9de9a63413b86007f65d19cbf5f1e6a400b2e2ce8d37f96
+size 165
