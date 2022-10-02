9db79c54c791782f4307c5356cd3cee01efb19b4
JangJiseon <jsmeaningood@gmail.com>
2022-10-03 03:55:05 +0900

    ✨ mygardens 코드 리팩토링
    - mygardens 코드 리팩토링
    
    
    Former-commit-id: dcf06913d837bf3d404e9ba0e7975bf054c9a47b

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0d7f7ca0074e5c36c9325d1e61fa4badad171be47214f1d8fb0d86f115ddf1
-size 4020
+oid sha256:a2c00c6f9cfba41720f3d315afac197db2e237214c9a33be33cc9dac46879012
+size 4057
--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa49708e4b564ee11857d75796d65acd8016b7a681dee7ac3b3e490bb726b1e7
-size 14404
+oid sha256:470c8e1ef27a7d2dc01580c1709369e183bf6ad0c7d6a355413875f920928a31
+size 14486
