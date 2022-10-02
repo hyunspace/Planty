72aa0d6e3f475d8757230babdc28a8c594ec08fb
조현빈 <jo7hb@naver.com>
2022-10-02 14:11:34 +0900

    ✨ Intro Page
    - intro page 제작 및 반응형
    
    
    Former-commit-id: b69807fee2c5865f11ee051d3e338eed99bea428

--- frontend/src/pages/magazine/MagazinePage.js
+++ frontend/src/pages/magazine/MagazinePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4c285527bb9e0f6816a68e07d2d2f2b83d2a01636b306a45f6fdd0770784490
-size 10261
+oid sha256:91e5309cf808615a77bfc6c9cf558ae3d7f8c1d35181259ee7c7e07926b449dd
+size 6471
