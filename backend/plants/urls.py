812c59054379025c7e22bf851b24a71114059c1f
Hong Seong Mok <swff07183@gmail.com>
2022-10-02 20:32:24 +0900

    ✨ 키워드 검색
    - 키워드 검색
    
    
    Former-commit-id: d501b880ae07ff45f3344b723dd55c622fdd963d

--- backend/plants/urls.py
+++ backend/plants/urls.py
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cf7ade1c6e9dee0a4283ab15c0294fd8acebac157dbc79ad1e028102bb9d9c8b
-size 402
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+app_name = 'plants'
+
+router = DefaultRouter()
+router.register(r'', views.PlantViewSet, basename='')
+urlpatterns = [
+    path('petsafety/', views.PetSafetyViewSet.as_view({'get': 'list'})),
+    path('popular/', views.PopularViewSet.as_view({'get': 'list'})),
+    path('', include(router.urls)),
+]
