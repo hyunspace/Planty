61f78eeeab986f74ae1dae0a31ced760a1410cfc
Hong Seong Mok <swff07183@gmail.com>
2022-10-02 20:32:24 +0900

    ✨ 키워드 검색
    - 키워드 검색
    
    
    Former-commit-id: d93a20fe3e27a6808bd34d6450bb072d2f746833

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f7e16d751b1da10fe2bba87a43df0c87f7b6cbf6bb9a4082a9a143c935cc3b9
-size 5081
+oid sha256:9ded50c0b8dd85ed0373780a4b592036efa73e6959634558d16fa2c96ecaddda
+size 4896
--- backend/recommendations/views.py
+++ backend/recommendations/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02b5061f2d62d00e9f580100cb8fca9df5fe05e11016531e054af25e33e73b69
-size 6385
+oid sha256:3602ce3ed3050acc22b0d141ec25e2361d4cc80533d72ab3f17d315f2de73043
+size 8236
