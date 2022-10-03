db3c5981329cf5b93273b24964aa7248b4f09735
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 01:33:35 +0900

    ✨ 피드 상세페이지 날짜 파싱
    - 피드 상세페이지 날짜 파싱
    - 내용2
    
    
    Former-commit-id: 299c94c6bb65ea0f79bc3cb2bc0b3ea20f1790d8

--- frontend/src/components/feed/FeedModal.js
+++ frontend/src/components/feed/FeedModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:523718b65351f0b5c8af7b52b2cf0b21b1a73e2d3135616e76cc2453013d1893
-size 12079
+oid sha256:0fce63bd679fc88a8361659f1ac12c224061d721dce702de09c115cf8d17dbe4
+size 12115
--- frontend/src/pages/user/ProfileUpdatePage.js
+++ frontend/src/pages/user/ProfileUpdatePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9cd4f627d3e8be7e6cfa4578c664895305a08385ad066e3b9baff1b1bff99bb
-size 5508
+oid sha256:ec2fab277e8b929c0ce8d882885c082950983fb6ff9b00029978fc890209693c
+size 5502
