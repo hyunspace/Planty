313db7e2d662113012c8bca7989a1eb7dbe72314
JangJiseon <jsmeaningood@gmail.com>
2022-10-03 16:47:07 +0900

    ✨ 나의 정원 식물 등록
    - 나의 정원 식물 등록
    
    
    Former-commit-id: 9b797796124eabb54fb71b3fdf265f0d78a59240

--- frontend/src/components/garden/GardenCreateModal.js
+++ frontend/src/components/garden/GardenCreateModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d255ed80d8482c8b557712e07c94ed47753cb551b625f7911799641fac3daac
-size 5150
+oid sha256:c7c982a9976097ed16bf3139ed21fbe0419b44f5d7223a70a766e806db5ec4a6
+size 5622
