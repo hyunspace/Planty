878ab8b0de083ab4bbbe410c0144df0fe7aede6a
Hong Seong Mok <swff07183@gmail.com>
2022-10-03 21:17:37 +0900

    ✨ 다이어리, 나의정원 get
    - 다이어리 get
    - 나의정원 get
    
    
    Former-commit-id: 385223855ebf75c20f442db877b02bd93e6f9e68

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228f3cae41f0067971870b55db48681750adbbf292db2062350742615b376d81
-size 12771
+oid sha256:11fa6eee1abb3ef50940c4c2929444ac640e1ee529bd2521c680a7ef08ba8650
+size 12996
