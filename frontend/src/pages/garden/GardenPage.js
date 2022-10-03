4e35d867387915d6cfd287c6401efb940db20d4e
Hong Seong Mok <swff07183@gmail.com>
2022-10-03 20:46:46 +0900

    ✨ 피드 모달 수정
    - 피드 모달 수정
    
    
    Former-commit-id: 69c8e0869fde477a725337a5775b76bbf776441e

--- frontend/src/pages/garden/GardenPage.js
+++ frontend/src/pages/garden/GardenPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e5c932442d8f675d6dac7a14df0adef7026ba8fe8dc4b063cfd19bade691df3
-size 2983
+oid sha256:af824738d7fa1e6caf067ac8e91e7d35fc7975488ed7b003060ad7bdb13a9878
+size 3277
