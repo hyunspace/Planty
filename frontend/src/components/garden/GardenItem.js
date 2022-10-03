52dd29ec9b54666ffebfab657e2972a7c98042fc
Hong Seong Mok <swff07183@gmail.com>
2022-10-04 01:02:14 +0900

    ✨ 피드 좋아요 추가
    - 피드 좋아요 추가
    
    
    Former-commit-id: 1a31af1cbad351a04f4de5fb293e9a5f0266faae

--- frontend/src/components/garden/GardenItem.js
+++ frontend/src/components/garden/GardenItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffae1b1606ad2cbadf8ae635c534e51a31c181e9b901cbf144cca7b1b6f8f396
-size 687
+oid sha256:af01cfba9ec750d0f2b62df1d0fdf2a45bf7f999fd34140e55472fb26741de15
+size 747
