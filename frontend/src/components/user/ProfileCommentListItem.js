3a0e90a49c9c12d381a25048e3f75103f39cf99f
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 20:53:05 +0900

    ✨ 마이페이지 나의 댓글, 좋아요한 글
    - 마이페이지 나의 댓글, 좋아요한 글 목록 띄우기
    - detail 페이지로 navigate
    
    
    Former-commit-id: 8671339ab6075cd51ee29509d6693c74bd7c8032

---
+++ frontend/src/components/user/ProfileCommentListItem.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9365c3f80782587e1df509f3e4f1ab62289916e6ac0cd2ad0494a4fecb895b56
+size 3318
--- frontend/src/pages/user/ProfilePage.js
+++ frontend/src/pages/user/ProfilePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e79d1153c8940dc4dd67bf47c73aa938da4c84bd175c8fa40a218ef55b68205
-size 4029
+oid sha256:c4d5124d042d7ca23de94f0866c2ac9ed6165b8a3817f4413d7937737dd7d3ee
+size 3980
