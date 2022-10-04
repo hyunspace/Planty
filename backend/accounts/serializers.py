4d2577e7d23ef6e88dcf39d44978e61b3e5d2f8e
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 10:44:41 +0900

    ✨ 유저 pk 같이 넘기기
    - 유저 pk 같이 넘기기
    
    
    Former-commit-id: 70c7cbbd91ffe8f4ec993a1abea3665d6b7c3ee8

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7978fd60ef79beeaa22d0775d0c5b1b6202fe77806dbd2e6b624ded430347cb8
-size 1494
+oid sha256:40212cbbaa61dc5b6cd4536a45ae4ae8dd248e9f8eea46924a7dbf297f8a3413
+size 1500
