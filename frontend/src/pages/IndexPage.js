1b5a4ba95cca6dc07584e539ebabd169d6862ec5
조현빈 <jo7hb@naver.com>
2022-10-04 16:25:14 +0900

    ✨ 컨플릭트 수정
    - 컨플릭트 수정
    
    
    Former-commit-id: 47488a2743c5568bd4d02ce973d0adc509e90dbc

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab5cf20b1a6797be01488a34ec960cd7b8f7e5d48f06f1fe964ffc1b4eb3ff47
-size 4430
+oid sha256:883aed0705aa6e41e5964d9a0d78c8adb8da550af9a879c5c7b2e2022c2983cd
+size 4273
