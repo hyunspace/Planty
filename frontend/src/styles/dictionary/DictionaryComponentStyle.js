ecd761477dae8f53643cafc63bff58b7fb5aec0c
조현빈 <jo7hb@naver.com>
2022-10-04 17:15:10 +0900

    ✨ IndexPage, DictionaryPage 애니메이션 추가
    - IndexPage 애니메이션 추가
    - DictionaryPage 애니메이션 추가
    
    
    Former-commit-id: e859918e5c1ea59bd0f72a0317f85bad961548cd

--- frontend/src/styles/dictionary/DictionaryComponentStyle.js
+++ frontend/src/styles/dictionary/DictionaryComponentStyle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9d31679d808db75d0323c650b678efc5a9a0ff803cfeafed4fc791fdae88844
-size 596
+oid sha256:e1ea7fb945bcad2ffd4715c7e81bd251d5b0e6dcf1224f8261e0bb4c97a5e794
+size 683
