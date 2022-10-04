4b5d31796a0f2d711ccc1ee96cdb6664f02ee64f
조현빈 <jo7hb@naver.com>
2022-10-04 16:23:51 +0900

    ✨ 애니메이션 추가
    - 인덱스 페이지 애니메이션 추가
    
    
    Former-commit-id: c0a9e689cf24a508263776511da2336cc577ea1e

--- frontend/src/components/TagButton.js
+++ frontend/src/components/TagButton.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7356fca9d09f66fac35af06e486ad7beddddd0eb3e98060f1b5780f82c9a5399
-size 797
+oid sha256:4695be62d0e0df56157ad8b5662383547a7432d93c30c359d2da5821d31d31a0
+size 955
