912df009252efead0edcb3cc83d6ad48de30fee9
조현빈 <jo7hb@naver.com>
2022-10-04 14:06:50 +0900

    ✨ 회원정보 수정
    - 회원정보 수정
    
    
    Former-commit-id: 4ae3cbc93c7fd9b532730e6395c6d4dca4c1f954

--- frontend/src/components/magazine/CommentLine.js
+++ frontend/src/components/magazine/CommentLine.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7a5101651ff6e5cd282b7790c1670b3de9cb51663c0fcd58ab9e4f3927824e3
-size 2997
+oid sha256:8621c6fe56aa9f89b2b36e3ea6733092affdcd362cc9ef3358388664b9f3757a
+size 3030
