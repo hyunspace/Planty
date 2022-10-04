07a5ed93304cd1fce40a258fb5f2181b44ed2f6e
이수민 <sumin9366@naver.com>
2022-10-04 15:11:05 +0900

    Update first_cal_sim.py
    Former-commit-id: d8c77ff8b1c28127e24290bd0a7159214dd02d01

--- DP/Algo/first_cal_sim.py
+++ DP/Algo/first_cal_sim.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b588721b09f0081b81ca0f74f2524c3d48cd4f787dcfc3567f3b8615a3d11f4f
-size 1397
+oid sha256:12c525c17122b9aa163e2d33d1be5001d7460bb8653ee69c62157fe99d12096f
+size 1396
