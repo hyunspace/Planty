68692b07e7ab41f5f857eb06eb21d6b32dcb503a
Hong Seong Mok <swff07183@gmail.com>
2022-10-04 15:23:49 +0900

    ✨ 태그 선택 효과 추가
    - 태그 선택 효과 추가
    
    
    Former-commit-id: 22c9654d2615116b71a4880343517e9e7ea3550b

--- frontend/src/components/dictionary/DictionaryTag.js
+++ frontend/src/components/dictionary/DictionaryTag.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5bb3af1d0328fe83b5790ac54a8a851b46c55e502517ea607ab3aa3ee53cbe4
-size 681
+oid sha256:646fd72781f9400fa7b15ad93cb9a0db40be95bb6bc9757c4d4b9b69e6a54a4d
+size 1198
--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79753a3ccebb17fbc9f37e5382e99eca053bff3ffc66a42126f7ea04bb693d51
-size 3930
+oid sha256:4085f8eb23657b5338d2ea84db176244c17babf704eb4b307cd14eded36aff89
+size 4148
