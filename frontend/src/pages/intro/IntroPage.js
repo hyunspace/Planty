0279444c60f9fffb7a566ccd5c50f62495cc39ce
조현빈 <jo7hb@naver.com>
2022-10-04 17:35:12 +0900

    ✨ header index 수정
    - header z-index 수정
    
    
    Former-commit-id: 4ef4f96ddf40b4a3ffaf244d59d74af749a9c00b

--- frontend/src/pages/intro/IntroPage.js
+++ frontend/src/pages/intro/IntroPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04a401a6da4cdf1102cd3ef882f532abcba6f548de357bd7ede9cbec52c305ba
-size 3471
+oid sha256:4ce04a5505afc9b4dbca719c8cc3a782b5c6492c226c01777eeafd2ac6fc5fd9
+size 3522
