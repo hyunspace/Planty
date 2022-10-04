16d337400a0ee075408d8f6a088562018c2dd621
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 23:36:32 +0900

    ✨ 비로그인 -> 나의 정원 클릭 시 로그인 페이지로 이동
    - 비로그인 -> 나의 정원 클릭 시 로그인 페이지로 이동
    
    
    Former-commit-id: 3548fb20e686a6c4ed9f0a5caa5d9cfcd32a047f

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dadf63e4f38f6db7c76a2deb0d4520c8bfe9ffc6fdb0f1881f29f4a79679e5e
-size 4044
+oid sha256:33c22525b88e663de8690477362ddd4074f362094f3e0913651635c30e80cf3a
+size 4289
