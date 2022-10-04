ce10e56862683e18066a3b5253388cbbf54d9fcb
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 16:53:57 +0900

    ✨ 피드 댓글 UD
    - 피드 댓글 UD
    
    
    Former-commit-id: c05801979d19707ddea66bf2a64829bffd226d32

--- frontend/src/components/feed/FeedModal.js
+++ frontend/src/components/feed/FeedModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e25b7e7aa3518f524416ebb493e5322ffe1a14a03a86a93c35cd77025733cde
-size 6352
+oid sha256:18afae954f5362ed5abb3ab718c3f4de744724d95fbfa74ff9783e70d8e04e3d
+size 8592
