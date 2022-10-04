f7d08a8ac1bb735154c7b133b842725a27cb064a
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 21:03:43 +0900

    ✨ 피드 경로 수정
    - 피드 경로 수정
    
    
    Former-commit-id: 0ed25b578034b9c812e33142a10f2518ff0dbb46

--- frontend/src/components/user/ProfileLikeListItem.js
+++ frontend/src/components/user/ProfileLikeListItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c242e0bffb2d76f013bb71202d0b6c181a2303f4cb34554b26a89460dc244c20
-size 2597
+oid sha256:def366bb7e25fd71976d9fdb40baae9f314ed02276d1920388b276524476ce2f
+size 2592
