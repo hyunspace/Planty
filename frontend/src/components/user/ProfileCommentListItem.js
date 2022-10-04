5aecc12c734e6934b5eb09c36836c68e63af8ca1
JangJiseon <jsmeaningood@gmail.com>
2022-10-05 00:14:55 +0900

    ✨ 남의 정원 로그인 이동
    - 비로그인 상태로 좋아요 클릭 시
    - 비로그인 상태로 댓글 등록 클릭 시
    
    
    Former-commit-id: f17b5053108d667d07f5a91321e29128272c7645

--- frontend/src/components/user/ProfileCommentListItem.js
+++ frontend/src/components/user/ProfileCommentListItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6230d960cfdb44673818b62ed4cbffa2a81b380a994fd68dee4bb69e2510db1
-size 3148
+oid sha256:a7df67b05596e92a2d2123ce31db4332ffce1c515c08664ee343f59374bea710
+size 3046
