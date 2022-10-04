7698cee51716add69af5d21e530e3e8290f5cbf7
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 14:56:39 +0900

    ✨ 읽을거리 유저 코드 리팩토링
    - 읽을거리 유저 코드 리팩토링
    
    
    Former-commit-id: 2926df845ec33e55219df40b524899c38ee9c7c5

--- frontend/src/components/magazine/CommentInput.js
+++ frontend/src/components/magazine/CommentInput.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5222c9b6d338d7ddbf80f1ff18016637224d4aefe4b4c9f3103fdcbe51cbbd9
-size 1656
+oid sha256:7599aa597506e41de296f6acef2e842d1809fc976d458403ce53113b3fbdc468
+size 1667
