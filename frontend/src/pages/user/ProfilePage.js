2c86560fe7fce2121549ad1b8896a3f789033e8a
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 23:23:30 +0900

    ✨ 마이페이지 title active
    - 마이페이지 title active
    
    
    Former-commit-id: 65f24e62485c0e74593033ff945706e20c88de25

--- frontend/src/pages/user/ProfilePage.js
+++ frontend/src/pages/user/ProfilePage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4d5124d042d7ca23de94f0866c2ac9ed6165b8a3817f4413d7937737dd7d3ee
-size 3980
+oid sha256:ecf74019061e11d67ff75b2682d680e7c6a2825caa85c2399a17b57fef8baa06
+size 4176
