49537b73f835afaa1b999322ede205611b82b9a1
Hong Seong Mok <swff07183@gmail.com>
2022-10-04 19:49:11 +0900

    ✨ 매거진 업데이트 추가
    - 매거진 업데이트 추가
    
    
    Former-commit-id: a409611525155958118b9f6da44a02e5a77f8f18

--- backend/magazines/views.py
+++ backend/magazines/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d522e3f77499f8767d519f93e9ade7b1863e7e8c6c79741616bbcd2f144bb4e
-size 8429
+oid sha256:08629e3968a6b5abd7b040643f9955612213cfc74f3ded253541fb12fd3a82f0
+size 9158
