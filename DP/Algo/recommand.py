81e4e413c78292ea6bb335fc3578f1e1e3231310
이수민 <sumin9366@ssafy.com>
2022-10-04 14:45:24 +0900

    ✨ 포트변경 & requirement 추가
    - 내용1
    - 내용2
    
    
    Former-commit-id: 01ddb9f4333823b78542dae8a959660cf8300e00

--- DP/Algo/recommand.py
+++ DP/Algo/recommand.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dbd1dd0cf55ca45a7064724ee3ac80f7f235cdc2df9f08c905f0b0fe0f5b0d8
-size 2641
+oid sha256:65a15ef4e3f5ef5c5f0aa23fd654ff6a910a66158a6b8d1f9b78ea545e9d0e78
+size 2637
