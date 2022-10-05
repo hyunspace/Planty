2d8fd8376ea118540edb1f491e641ac46caf99b7
홍성목 <swff07183@gmail.com>
2022-10-05 16:47:51 +0900

    ✨ 유저 기반 추천 식물 추가
    - 유저 기반 추천 식물 추가
    
    
    Former-commit-id: 99705f25525300bbb030e36efeb3500bbcc4c6b4

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42adce5b9207c6668860447a0d0e15f8e31a78ccf01f7e00e1e45cafed5e0a80
-size 4573
+oid sha256:bb8ea925098b4b86eb4d9a0bbbe1f0a5ce28f87347654595727b91c2e2bdeba7
+size 4667
