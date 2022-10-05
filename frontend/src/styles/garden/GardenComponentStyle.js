f0ad35f19275d1da55545748927179986e8b3418
hyunspace <33j073@gmail.com>
2022-10-06 00:28:09 +0900

    ✨ 식물 일기 모달 CSS
    - 나의 정원 식물 일기 모달 CSS
    
    
    Former-commit-id: b075ecf283981aadf431848846520896438b5604

--- frontend/src/styles/garden/GardenComponentStyle.js
+++ frontend/src/styles/garden/GardenComponentStyle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b3a3589e2b5c586e18fbfabf182b06cdf170f18a4d5598d4f44082502d31591
-size 11485
+oid sha256:fc19305cab279e8ec43364ed9949f2d85bc2e8bf2d5da13abaca6ad25e68594e
+size 12317
