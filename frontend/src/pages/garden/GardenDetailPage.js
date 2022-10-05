9098181edf924328f8bd76263c31bc51390698a8
조현빈 <jo7hb@naver.com>
2022-10-05 23:34:45 +0900

    ✨ 나의 정원 식물 삭제, 나의 정원 식물 일기 삭제
    - 나의 정원 식물 삭제 구현 -> 500 error
    - 나의 정원 식물 일기 삭제 구현
    
    
    Former-commit-id: 0031a0d6159eba93552241def647dcf4c9166641

--- frontend/src/pages/garden/GardenDetailPage.js
+++ frontend/src/pages/garden/GardenDetailPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:befc96bc99803123f367895f5316d7c2d94eb4b1e2618cbe41767cb6cbc462c8
-size 4132
+oid sha256:2a5551cab301b83d12f9777993afb22574dcb827411d69430a8a8fe5797732e1
+size 4392
