2e97c4c8565b334b123454472ae35cf4c86d40bd
hyunspace <33j073@gmail.com>
2022-10-06 02:29:55 +0900

    ✨ 나의 정원 상세 페이지 CSS
    - GardenDetailPage styling
    
    
    Former-commit-id: 67c9eb882a289796db8cf4c802ac36f07457716d

--- frontend/src/pages/garden/GardenDetailPage.js
+++ frontend/src/pages/garden/GardenDetailPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f89926d25d017edbb2a2ff38ced13ca5c1763be392f7524dd1966786b6218fb8
-size 4495
+oid sha256:36140c092b29e4a5f2961149a89303e17218225cb7ce8e6df358605b49bf1333
+size 4703
