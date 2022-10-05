bd19ad3d52dcb0a409a93bacf09f61bcc38d64aa
JangJiseon <jsmeaningood@gmail.com>
2022-10-05 11:40:57 +0900

    ✨ 식이월 주석 제거
    - 식이월 주석 제거
    
    
    Former-commit-id: 123d6f7a1b2bffd924046937059cedb495b80c91

--- backend/mygardens/views.py
+++ backend/mygardens/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b494954fe81399965d87c9fc0fc96aa21a941972807d1907c3bbda99bccf77f1
-size 15078
+oid sha256:02d6b125b528d22d55b04a624d16cda94c5fd6d430c0704c577548edb6d554e6
+size 14924
