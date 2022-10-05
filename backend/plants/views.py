61bd2d193b6604fb6143bda09d7fc9c3de79537d
홍성목 <swff07183@gmail.com>
2022-10-05 16:09:06 +0900

    ✨ 펫 식물 수정
    - 펫 식물 수정
    
    
    Former-commit-id: 2eb5eb3c1f9467947f812d8745f8217c6763f387

--- backend/plants/views.py
+++ backend/plants/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:765cba8d90a828bb881a9b0bde9fa09b39b9d33c1822af02a8a532b305a8df0e
-size 4537
+oid sha256:ea6e4f6c6c3e4af8b24992222dd0d35bbbb97ddc94fc180bd0dbfff6ea2321ff
+size 4539
