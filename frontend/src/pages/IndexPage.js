0d9cb2974c1edfa61da9400a3784de5ae29c0731
조현빈 <jo7hb@naver.com>
2022-10-05 14:31:01 +0900

    ✨ 나의 정원 모달 및 디자인, 레이아웃 수정
    - 나의 정원 모달 크기 수장
    - 나의 정원 디자인 변경
    - 나의 정원 레이아웃 수정
    
    
    Former-commit-id: 97c263a832f333f3917b37973882801bb1468fa4

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:281994fb5c7d83cb16d461deeda269aa09d5e36c2e8df461fbbe1d3aa45ee156
-size 4541
+oid sha256:dce9f041093957317f06b90b003337c68b2d4add42bda62a9e75dbe2771737c1
+size 4508
