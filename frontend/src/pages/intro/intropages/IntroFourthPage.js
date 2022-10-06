f6c415b14f0246b5be4cb07b45fec4e56c4b4c2a
조현빈 <jo7hb@naver.com>
2022-10-07 00:36:59 +0900

    ✨ gif to webp, 월드컵 타이틀, 폰트 수정
    - gif 파일 webp 파일로 변환
    - 월드컵 타이틀 한글로 변경
    - 폰트 기본으로 변경
    
    
    Former-commit-id: d0daf9f603b2ab55912115d72771feb30663ba9f

--- frontend/src/pages/intro/intropages/IntroFourthPage.js
+++ frontend/src/pages/intro/intropages/IntroFourthPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32697fa542e914543bde2a74827f9c3573a29ea2f43badcdbe4987f7943b98d4
-size 819
+oid sha256:875840a12026696721cee0b913b34a9da5ef98722b01a432dffbce5efe119bc6
+size 820
