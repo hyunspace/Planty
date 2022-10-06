c08bee97a89ecc11ad226be2f87ecef802fecffe
Hong Seong Mok <swff07183@gmail.com>
2022-10-07 02:47:44 +0900

    ✨ ckeditor 수정
    - ckeditor 수정
    
    
    Former-commit-id: 1852467ab98d99b48dec004d53e553579aae0569

--- frontend/src/components/CKEditor5.js
+++ frontend/src/components/CKEditor5.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bce22da87faa692eac935e633611b1fa0e441c79328ecbaf94db43954bec7b8d
-size 1606
+oid sha256:de5514bc8d8a839c4879db4805edec68a7de09b02fc03bdf97244274cdd16fda
+size 1521
