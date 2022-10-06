1b7dbbae02ab3cd42de2c2840964a90fcc61d213
Hong Seong Mok <swff07183@gmail.com>
2022-10-07 01:25:41 +0900

    🐛 무한로딩 수정
    - 무한로딩 수정
    
    
    Former-commit-id: 2e5dbc0c779d490c50fd7f230418e1bed8cccfb0

--- frontend/src/pages/user/RegisterPage.js
+++ frontend/src/pages/user/RegisterPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e444a916751dfc14e4d98a2c9ddcb0284770d33c8637cae284e5e97190e0577
-size 6546
+oid sha256:e415e7bcfd7fdc5bedb6a6d8be00fa9df6d207134f6780691f3838fe6832dbe8
+size 6521
