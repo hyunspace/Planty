02ce65e099436ccd0ac3c00b4b09bd66c872a472
조현빈 <jo7hb@naver.com>
2022-10-07 02:01:42 +0900

    ✨ CKEditor5 수정
    - CKEditor5 수정
    
    
    Former-commit-id: 8e9cd224b70461a82fb187e3972e512577c9ba42

--- frontend/src/components/CKEditor5.js
+++ frontend/src/components/CKEditor5.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d07005f92785cd4c524897d6d73772f41936655df76424405bd65f0d5613e7
-size 533
+oid sha256:93ddd4f59c0f943e6f30b23fd30830aaf5c442aee4d5e7ca960ac17f37d921cd
+size 1339
