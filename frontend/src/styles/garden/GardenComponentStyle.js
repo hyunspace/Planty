49de93fd0e7e0a55cefbd77b8979abbcd6c0e609
JangJiseon <jsmeaningood@gmail.com>
2022-10-06 13:13:27 +0900

    ✨ description css
    - description css
    
    
    Former-commit-id: 44abb02dc90af7d53b9d4bba3149c079463064b7

--- frontend/src/styles/garden/GardenComponentStyle.js
+++ frontend/src/styles/garden/GardenComponentStyle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b3a3589e2b5c586e18fbfabf182b06cdf170f18a4d5598d4f44082502d31591
-size 11485
+oid sha256:fc5ac56658e707c172517e9b5e966c670cc3ae7fa7669197c8a1d309ea4bc6d7
+size 11577
