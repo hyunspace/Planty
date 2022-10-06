04b8506b60c0ba8ee74cb6136fdc8483a49f3a44
JangJiseon <jsmeaningood@gmail.com>
2022-10-06 16:20:47 +0900

    ✨ 유저 프로필 사진 변경
    - 유저 프로필 사진 변경
    
    
    Former-commit-id: a2a25a9af46f83e8e65772a186821a10be150d5c

--- frontend/src/components/user/ProfileUpdateModal.js
+++ frontend/src/components/user/ProfileUpdateModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2822238947bc497924a47217279f7eadaad371c58d605df256776170d49658bb
-size 7654
+oid sha256:91a4d243ca393f299e069d6429f9fe69b5f8fcaf449a0dd3be7279b4b35848e5
+size 7911
