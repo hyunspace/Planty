9b7235de3127887fb2955924a8a2a0c55b934000
조현빈 <jo7hb@naver.com>
2022-10-07 03:12:22 +0900

    ✨ CKEditor5 수정 끗!
    - CKEditor5 수정 끗!
    
    
    Former-commit-id: e2ad69a3c8e80b3c6d2330093f4188fec7dfaf2f

--- frontend/src/pages/magazine/MagazineInputPage.js
+++ frontend/src/pages/magazine/MagazineInputPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:202818bf2668634c9a70f8f7a58239a2bf34c2f3498f9a6c47a0814f8c3a04ce
-size 2985
+oid sha256:10fc34f6cc5f6f778c2418a800367f1cfe677769c14ffabe4a79dc147c79aaf5
+size 3361
