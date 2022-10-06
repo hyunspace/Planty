4c1548b7a3a4668c1a0d1e7ece8dde839addbbe0
JangJiseon <jsmeaningood@gmail.com>
2022-10-07 00:29:45 +0900

    ✨ 로그인 & 회원가입 한글화
    - 내용1
    - 내용2
    
    
    Former-commit-id: d6ff532290a986d6ab0efc90437b5b154ba820ac

--- frontend/src/pages/user/LoginPage.js
+++ frontend/src/pages/user/LoginPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff1424418a27cf0323ff5a0886e3adfdd924f72bc433e6f1d9be554b5345359e
-size 2736
+oid sha256:d7b9f6a2389fcb513c6e5a536094be82bce31c77315d79f910a6ed4c2d026468
+size 2770
